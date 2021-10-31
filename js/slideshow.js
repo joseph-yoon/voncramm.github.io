d517a9349a0bf475f892220d1aad3b8fdfa04357
redbudgithubsec <79767169+redbudgithubsec@users.noreply.github.com>
2021-10-31 16:55:43 -0400

    Update slideshow.js
    pic name update

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var images = [
     "images/voncramm31.jpg", 
     "images/vcfront.jpg",
-    "images/vcsecond.JPG",
+    "images/vcsecond.jpg",
     "images/vcthird.jpg", 
     "images/vc5.jpg"
   ];
